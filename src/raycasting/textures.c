734cf9c66ea018952472c046411176e0ef8ffd86
Jack Mcgrane <jmcgrane@f1r5s15.codam.nl>
2026-02-20 14:17:16 +0100

    textures

--- src/raycasting/textures.c
+++ src/raycasting/textures.c
@@ -6,7 +6,7 @@
 /*   By: jmcgrane <jmcgrane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/20 12:42:42 by jmcgrane          #+#    #+#             */
-/*   Updated: 2026/02/20 13:59:22 by jmcgrane         ###   ########.fr       */
+/*   Updated: 2026/02/20 14:17:00 by jmcgrane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,5 +28,15 @@ static mlx_texture_t	*pick_texture(t_game *game, t_ray *ray)
 		else
 			return (game->texture.north);
 	}
+	return (game->texture.north);
 }
 
+static uint32_t	get_tex_pixel(mlx_texture_t *tex, int x, int y)
+{
+	uint8_t	*pixel;
+
+	if (x < 0 || x >= (int)tex->width || y < 0 || y >= (int)tex->height)
+		return (0);
+	pixel = &tex->pixels[(y * tex->width + x) * tex->bytes_per_pixel];
+	return (pixel[0] << 24 | pixel[1] << 16 | pixel[2] << 8 | pixel[3]);
+}
