8e320ed90747b5038591b9ca00a58d98dcefb215
Jack Mcgrane <jmcgrane@f1r6s12.codam.nl>
2026-03-02 14:08:24 +0100

    x

--- obj/parsing/parse_map.d
+++
@@ -1,9 +0,0 @@
-obj/parsing/parse_map.o: src/parsing/parse_map.c \
-  src/parsing/../../inc/cub3d.h inc/../lib/libft/libft.h \
-  lib/MLX42/include/MLX42/MLX42.h
-
-src/parsing/../../inc/cub3d.h:
-
-inc/../lib/libft/libft.h:
-
-lib/MLX42/include/MLX42/MLX42.h:
