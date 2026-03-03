4369df06128e9fcdbd158669a585588cf80c15fa
adecheri <adecheri@student.codam.nl>
2026-03-03 19:15:02 +0100

    clean upload

--- obj/parsing/read_map.d
+++
@@ -1,9 +0,0 @@
-obj/parsing/read_map.o: src/parsing/read_map.c \
-  src/parsing/../../inc/cub3d.h inc/../lib/libft/libft.h \
-  lib/MLX42/include/MLX42/MLX42.h
-
-src/parsing/../../inc/cub3d.h:
-
-inc/../lib/libft/libft.h:
-
-lib/MLX42/include/MLX42/MLX42.h:
