ce3264fa0ada3283836086fe9caa37cd91c52e4b
Jack Mcgrane <jmcgrane@f1r5s15.codam.nl>
2026-03-03 13:19:47 +0100

    segfault on SO

--- obj/raycasting/init_player.d
+++
@@ -1,9 +0,0 @@
-obj/raycasting/init_player.o: src/raycasting/init_player.c \
-  src/raycasting/../../inc/cub3d.h inc/../lib/libft/libft.h \
-  lib/MLX42/include/MLX42/MLX42.h
-
-src/raycasting/../../inc/cub3d.h:
-
-inc/../lib/libft/libft.h:
-
-lib/MLX42/include/MLX42/MLX42.h:
