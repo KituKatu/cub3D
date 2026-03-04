94ff28681cd6964fb63be0bcf4eeddc3cfd701b7
Jack Mcgrane <jmcgrane@f1r5s15.codam.nl>
2026-03-04 14:20:13 +0100

    new textures

--- obj/utils/free.d
+++
@@ -1,8 +0,0 @@
-obj/utils/free.o: src/utils/free.c src/utils/../../inc/cub3d.h \
-  inc/../lib/libft/libft.h lib/MLX42/include/MLX42/MLX42.h
-
-src/utils/../../inc/cub3d.h:
-
-inc/../lib/libft/libft.h:
-
-lib/MLX42/include/MLX42/MLX42.h:
